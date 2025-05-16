66582d43424989b36d50c18d7937937b51dc06ae
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-05-16 17:47:26 +0200

    snack 1 e 2

---
+++ esercizi.js
@@ -0,0 +1,111 @@
+const books = [
+  { 
+	  title: "React Billionaire", 
+	  pages: 250, 
+	  author: {
+		  name: 'Alice',
+		  age: 35
+	  },
+	  available: false,
+	  price: '101€',
+	  tags: ['advanced', 'js', 'react', 'senior']
+  },
+  { 
+	  title: "Advanced JS", 
+	  pages: 500, 
+	  author: {
+		  name: 'Bob',
+		  age: 20
+	  },
+	  available: true,
+	  price: '25€',
+	  tags: ['advanced', 'js', 'mid-senior']
+  },
+  { 
+	  title: "CSS Secrets", 
+	  pages: 320, 
+	  author: {
+		  name: 'Alice',
+		  age: 17
+	  },
+	  available: true,
+	  price: '8€',
+	  tags: ['html', 'css', 'junior']
+  },
+  { 
+	  title: "HTML Mastery", 
+	  pages: 200, 
+	  author: {
+		  name: 'Charlie',
+		  age: 50
+	  },
+	  available: false,
+	  price: '48€',
+	  tags: ['html', 'advanced', 'junior', 'mid-senior']
+  },
+];
+
+
+// ------------Snack 1------------
+
+// 1. Funzione che somma due numeri
+
+function sommaNumeri(a,b) {
+    return a + b;
+    
+}
+
+// ------------------------------------------------------------
+
+// 2. Array con i libri che hanno più di 300 pagine
+
+const libriLunghi = books.filter(books => books.pages > 300)
+// console.log(libriLunghi);
+
+// ------------------------------------------------------------
+
+
+// 3. Array con i titoli dei libri in longBooks
+
+const libriLunghiTitoli = libriLunghi.map(book => book.title)
+// console.log(libriLunghiTitoli);
+
+// ------------------------------------------------------------
+
+// 4. Stampa ogni titolo in console
+
+books.forEach(title => console.log(title))
+
+// ------------Snack 2------------
+
+// 1. Creare un array con i libri disponibili
+const libriDisponibili = books.filter(b => b.available === true)
+// console.log(libriDisponibili);
+
+// ------------------------------------------------------------
+
+// 2. Creare un array con i libri scontati del 20%
+
+// 2. Creare un array con i libri scontati del 20%
+const libriScontati = libriDisponibili.map(book => {
+    const prezzo = parseFloat(book.price);
+    const prezzoScontato = Math.round(prezzo * 0.8 * 100) / 100;
+    return {
+        ...book,
+        price: `${prezzoScontato}€`
+    };
+});
+console.log(libriScontati);
+
+
+
+// ------------------------------------------------------------
+
+// 3. Trovare il primo libro con prezzo intero (senza centesimi)
+const libriPrezzoPieno = libriScontati.find(book => {
+    const price = parseFloat(book.price)
+    return Number.isInteger(price);
+});
+// console.log(libriPrezzoPieno);
+
+// ------------Snack 3------------
